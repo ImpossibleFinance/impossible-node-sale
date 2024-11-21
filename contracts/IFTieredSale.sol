241b7f0b1073b0a02ccc87a9903e1643a7fc2bea
friday-james <lamcollin37@gmail.com>
2024-11-20 21:54:01 -0800

    avoid unexpected reverts due to underflows

--- contracts/IFTieredSale.sol
+++ contracts/IFTieredSale.sol
@@ -609,10 +609,10 @@ contract IFTieredSale is IFFundable, AccessControl {
    }
     // view function for ops
     function getAllPromoCodeInfo(uint256 fromIdx, uint256 toIdx) external view returns (PromoCode[] memory) {
-        require(fromIdx < toIdx, "Invalid range");
         if (toIdx > allPromoCodes.length) {
             toIdx = allPromoCodes.length;
         }
+        require(fromIdx < toIdx, "Invalid range");
         PromoCode[] memory promoCodeInfos = new PromoCode[](toIdx - fromIdx);
         for (uint i = fromIdx; i < toIdx; i++) {
             promoCodeInfos[i - fromIdx] = promoCodes[allPromoCodes[i]];
@@ -625,10 +625,10 @@ contract IFTieredSale is IFFundable, AccessControl {
     }
 
     function getAllPromoCodes(uint256 fromIdx, uint256 toIdx) external view returns (string[] memory) {
-        require(fromIdx < toIdx, "Invalid range");
         if (toIdx > allPromoCodes.length) {
             toIdx = allPromoCodes.length;
         }
+        require(fromIdx < toIdx, "Invalid range");
         string[] memory promoCodeList = new string[](toIdx - fromIdx);
         for (uint i = fromIdx; i < toIdx; i++) {
             promoCodeList[i] = allPromoCodes[i];
